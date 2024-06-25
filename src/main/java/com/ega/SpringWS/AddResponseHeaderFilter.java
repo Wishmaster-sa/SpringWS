788147156ed180eb875692ab17ba5d3394f05e00
sa <sa@ubuntu-linux-22-04-02-desktop>
2024-06-25 16:55:57 +0200

    додав заголовки які додають безпеку API в відповідь сервісу Cache-Control: no-store Content-Security-Policy: frame-ancestors 'none' Strict-Transport-Security: max-age=63072000; includeSubDomains; preload X-Content-Type-Options: nosniff X-Frame-Options: DENY

--- src/main/java/com/ega/SpringWS/AddResponseHeaderFilter.java
+++ src/main/java/com/ega/SpringWS/AddResponseHeaderFilter.java
@@ -21,6 +21,16 @@ import java.io.IOException;
 //якщо потрібно до кожної відповіді додавати якусь інформацію, то це робиться за допомогою WebFilter.
 //@WebFilter("/filter-response-header/*")
 //Додаємо до кожної відповіді заголовок Access-Control-Allow-Origin (щоб браузер не лаявся на CORS)
+//Додаємо заголовок Cache-Control: no-store який вказує, що будь-які кеші будь-якого типу (приватні або загальні) не повинні зберігати відповідь, що містить заголовок
+//Додаємо заголовок Content-Security-Policy": "frame-ancestors 'none' Заголовок, що використовується для вказівки,
+//чи може бути відповідь укладена в елемент <frame>, або ні. Цей заголовок захищає від атак клікджекінгу
+//Додаємо заголовок X-Content-Type-Options: nosniff Заголовок, що вказує браузеру завжди використовувати тип MIME,
+//оголошений у Content-Type заголовку, а не намагатися визначити тип MIME на основі вмісту файлу. 
+//Цей заголовок з nosniff значенням не дозволяє браузерам виконувати аналіз MIME та некоректно інтерпретувати відповіді як HTML
+//X-Frame-Options: DENY Заголовок , що використовується для вказівки, чи може бути відповідь укладена в елемент <frame>, або ні.
+//Для відповіді API не потрібно, щоб вона була укладена в будь-який з цих елементів.
+//Надання не дозволяє будь-якому домену формувати відповідь, що повертається викликом API.
+//Цей заголовок зі значенням захищає від атак клікджекінгу
 @WebFilter()
 public class AddResponseHeaderFilter implements Filter {
 
@@ -30,6 +40,16 @@ public class AddResponseHeaderFilter implements Filter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setHeader(
           "Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader(
+          "Cache-Control", "no-store");
+        httpServletResponse.setHeader(
+          "Content-Security-Policy", "frame-ancestors 'none'");
+        httpServletResponse.setHeader(
+          "Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
+        httpServletResponse.setHeader(
+          "X-Content-Type-Options", "nosniff");
+        httpServletResponse.setHeader(
+          "X-Frame-Options", "DENY");
         chain.doFilter(request, response);
     }
 }
