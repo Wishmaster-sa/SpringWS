fafd3e99c839d6e3aa381692d1cab7b2a6541774
Wishmaster-sa <ich.danyliuk@gmail.com>
2024-07-01 20:42:17 +0200

    Налаштування программи як сервіса
    Скрипт який допомогає використовувати програму як сервіс.
    Використання:
    1. Розгорнути середовище згідно інструкції
    2. Скомпілювати вихідні файли та отримати jar файл
    3. Зробить всі необхідни налаштування в файлі webservice.settings
    3. Зробити файл springws-service.sh виконуваним.
    4. З каталогу SpringWS запустити цей файл на виконання.
    bash springws-service.sh install
    Щоб скасувати інсталяцію програми як сервісу виконайте: bash springws-service.sh remove
    bash springws-service.sh start - запускає сервіс
    bash springws-service.sh stop - припиняє сервіс
    bash springws-service.sh - без параметрів видає меню стосовно дій цього сервіса.

---
+++ springws-service.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+
+function installService() {
+	echo 'Встановлюю сервіс...'
+	sudo mkdir /opt/SpringWS
+	sudo cp ./target/SpringWS-0.0.1-SNAPSHOT.jar /opt/SpringWS
+	sudo cp ./webservice.settings /opt/SpringWS
+	sudo cp ./springws.service /etc/systemd/system
+	sudo sudo systemctl daemon-reload
+	sudo systemctl enable springws.service
+	sudo systemctl start springws.service
+	sudo systemctl status springws.service
+}
+
+function removeService() {
+	echo "Видаляю SpringWS сервіс..."
+	sudo systemctl stop springws.service
+	sudo systemctl disable springws.service
+	sudo rm /etc/systemd/system/springws.service
+	sudo sudo systemctl daemon-reload
+	
+	sudo rm -R /opt/SpringWS
+}
+
+function startService() {
+	echo "Запускаю SpringWS сервіс..."
+	sudo systemctl start springws.service
+	sudo systemctl status springws.service
+}
+
+function stopService() {
+	echo "Припиняю SpringWS сервіс..."
+	sudo systemctl stop springws.service
+	sudo systemctl status springws.service
+}
+
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Для выполнения скрипта нужны права sudo, запустите скрипт с правами суперпользователя"
+    exit 1
+fi
+
+if [ -z $1 ]; then
+	PS3='Будь-ласка зробить вибір: '
+	select option in "Встановити сервіс" "Запустити сервіс" "Припинити сервіс" "Видалити сервіс" "Вихід"
+	do
+	    case $option in
+		"Встановити сервіс")
+		    installService
+		    break
+		    ;;
+		"Запустити сервіс")
+		    startService
+		    break
+		    ;;
+		"Припинити сервіс")
+		    stopService
+		    break
+		    ;;
+		"Видалити сервіс")
+		    removeService
+		    break
+		    ;;
+		"Вихід")
+		    break
+		    ;;
+		*)
+		    echo 'Invalid option.'
+		    ;;
+	    esac
+	done	
+	exit 1
+fi
+
+
+case $1 in
+    'install')
+        installService
+        ;;
+    'start')
+        startService
+        ;;
+    'stop')
+        stopService
+        ;;
+    'remove')
+        removeService
+        ;;
+    *)
+        echo 'Usage: bash springws-service.sh install|start|stop|remove'
+        ;;
+esac
+
