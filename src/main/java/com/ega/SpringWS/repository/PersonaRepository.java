46ceef2a8b01d90949df14947f4f3325aa879936
fortor48 <fortor48@gmail.com>
2024-07-06 14:35:17 +0300

    Update PersonaRepository.java (Changes in text)

--- src/main/java/com/ega/SpringWS/repository/PersonaRepository.java
+++ src/main/java/com/ega/SpringWS/repository/PersonaRepository.java
@@ -12,29 +12,30 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 /**
- * Це інтерфейс репозіторію - сутність яка вказує яки процедури та функції будє єкспорувати наша програма
- * для роботи з базою даних. Зверніть увагу, що основні функції SELECT, INSERT, UPDATE, DELETE тут не вказани
- * так як вони додаються автоматично бібліотекою JpaRepository. В JPA вони називаються find, save, save, delete.
- * Для додання інших функцій уважно читайте опис бібліотеки JPA 
+ * Це інтерфейс репозіторію - сутність, яка вказує, які процедури та функції буде експорувати наша програма
+ * для роботи з базою даних. Зверніть увагу, що основні функції SELECT, INSERT, UPDATE, DELETE тут не зазначені
+ * так як вони додаються автоматично бібліотекою JpaRepository. В JPA вони називаються find, save, save, delete відповідно.
+ * Для додавання інших функцій уважно читайте опис бібліотеки JPA
  * https://ru.hexlet.io/courses/java-spring/lessons/jpa-repository/theory_unit
  */
 public interface PersonaRepository extends JpaRepository<Persona, Long>{
 
-    //зверніть увагу, назва методу має значення, повинно бути не deleteByRNOKPP а deleteByRnokpp. Інакше JpaRepository не зможе взяти реалізацію цього метода на себе!
+    //зверніть увагу, що назва методу і регістр символів має значення, повинно бути не deleteByRNOKPP, а deleteByRnokpp. Інакше JpaRepository не зможе взяти реалізацію цього метода на себе!
     void deleteByRnokpp(String rnokpp);
     
-    //приклад пошуку персони по RNOKPP
+    //приклад пошуку користувача по RNOKPP
     Persona findByRnokpp(String rnokpp);
     
-    //приклад пошуку персони по імені
+    //приклад пошуку користувача по імені
     List <Persona> findAllByFirstName(String firstName);
     
-    //приклад пошуку персони по призвищу яке починається з заданного перфікса
+    //приклад пошуку користувача по прізвищу, яке починається з заданого перфікса
     List <Persona> findByLastNameStartingWith(String prefix);
-    
+
+    //приклад пошуку користувача по даті народження
     List <Persona> findByBirthDateBetween(LocalDate startAge, LocalDate endAge);
     
-    //приклад кастомного метода в якому напряму задається що щукати (всі імʼя яки мають в назві шаблон)
+    //приклад кастомного методу, в якому напряму задається що шукати (всі імена, які мають в назві шаблон)
     @Query("SELECT e FROM Persona e WHERE e.firstName LIKE %:firstName%")
     List <Persona> findAllByFirstNameContaining(@Param("firstName") String firstName);
 }
